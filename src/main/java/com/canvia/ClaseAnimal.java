e17cf4cf91b81f1a28f95c650a6daf4407bb8956
DIEGO <alonsomauricio1@gmail.com>
2019-04-11 18:39:57 -0500

    asdasas

--- src/main/java/com/canvia/ClaseAnimal.java
+++ src/main/java/com/canvia/ClaseAnimal.java
@@ -5,4 +5,16 @@ public class ClaseAnimal {
     public String concatenate(String one, String two){
         return one + two;
     }
+
+    public String concatenate2(String one, String two){
+        return one + two;
+    }
+
+    public String concatenate3(String one, String two){
+        return one + two;
+    }
+
+    public String concatenate4(String one, String two){
+        return one + two;
+    }
 }
--- src/test/java/com/canvia/ClaseAnimalTest.java
+++ src/test/java/com/canvia/ClaseAnimalTest.java
@@ -15,4 +15,34 @@ public class ClaseAnimalTest {
         assertEquals("onetwo", result);
 
     }
+
+    @Test
+    public void testConcatenate2() {
+        ClaseAnimal myUnit = new ClaseAnimal();
+
+        String result = myUnit.concatenate2("one", "two");
+
+        assertEquals("onetwo", result);
+
+    }
+
+    @Test
+    public void testConcatenate3() {
+        ClaseAnimal myUnit = new ClaseAnimal();
+
+        String result = myUnit.concatenate3("one", "two");
+
+        assertEquals("onetwo", result);
+
+    }
+
+    @Test
+    public void testConcatenate4() {
+        ClaseAnimal myUnit = new ClaseAnimal();
+
+        String result = myUnit.concatenate4("one", "two");
+
+        assertEquals("onetwo", result);
+
+    }
 }
